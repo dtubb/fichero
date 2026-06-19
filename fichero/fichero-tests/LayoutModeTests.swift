adffca6e797fa7f1c59e2b4efa6b8fe9e9b1814b
Daniel Tubb <dtubb@me.com>
2026-06-19 09:49:32 -0300

    test: extract adaptive shell policy coverage (#2330)

--- fichero/fichero-tests/LayoutModeTests.swift
+++ fichero/fichero-tests/LayoutModeTests.swift
@@ -212,53 +212,6 @@ final class LayoutModeTests: XCTestCase {
         XCTAssertEqual(resolved?.id, "image-folder")
     }
 
-    func testAdaptiveShellDefaultsAndLegacyRestoreStayPlatformSpecific() {
-        #if os(macOS)
-        XCTAssertEqual(ContentView.defaultColumnVisibility, .all)
-        XCTAssertEqual(ContentView.defaultColumnVisibilityRaw, 2)
-        XCTAssertEqual(ContentView.restoredColumnVisibility(from: 2), .all)
-        XCTAssertEqual(ContentView.persistedColumnVisibilityRaw(for: .all), 2)
-        #else
-        XCTAssertEqual(ContentView.defaultColumnVisibility, .detailOnly)
-        XCTAssertEqual(ContentView.defaultColumnVisibilityRaw, 1)
-        XCTAssertEqual(ContentView.restoredColumnVisibility(from: 2), .detailOnly)
-        XCTAssertEqual(ContentView.persistedColumnVisibilityRaw(for: .all), 3)
-        XCTAssertEqual(ContentView.restoredColumnVisibility(from: 3), .doubleColumn)
-        #endif
-    }
-
-    func testAdaptiveShellPersistenceKeepsCollapsedAndExplicitWideStatesDistinct() {
-        XCTAssertEqual(ContentView.restoredColumnVisibility(from: 0), .automatic)
-        XCTAssertEqual(ContentView.restoredColumnVisibility(from: 1), .detailOnly)
-        XCTAssertEqual(ContentView.persistedColumnVisibilityRaw(for: .detailOnly), 1)
-        XCTAssertEqual(ContentView.persistedColumnVisibilityRaw(for: .automatic), 0)
-    }
-
-    func testWindowMinimumSeparatesMacShellChromeFromCompactDetailLayout() {
-        let detailWidth = 600.0
-
-        #if os(macOS)
-        let expected = ContentView.sidebarMinWidth + detailWidth + ContentView.inspectorMinWidth
-        XCTAssertEqual(
-            ContentView.windowMinWidth(
-                sidebarVisible: true,
-                inspectorVisible: true,
-                detailMinWidth: detailWidth
-            ),
-            expected
-        )
-        #else
-        XCTAssertEqual(
-            ContentView.windowMinWidth(
-                sidebarVisible: true,
-                inspectorVisible: true,
-                detailMinWidth: detailWidth
-            ),
-            detailWidth
-        )
-        #endif
-    }
-
     func testImageBackedPageDoesNotUsePDFCanvas() {
         let imagePage = Document(
             id: "image-page",
